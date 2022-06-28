4f7294fa2388378856e037a022108dcc92020652
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-06-28 11:43:06 -0500

    added reselect to memoize selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,18 @@
+// a memoized selector that pulls values from the category reducer
+import {createSelector} from 'reselect';
 
-export const selectCategoriesMap=(state)=>
-    state.categories.categories
+const selectCategoryReducer=(state)=>state.categories;
+
+export const selectCategories=createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice)=>categoriesSlice.categories
+)
+
+export const selectCategoriesMap=createSelector([selectCategories],
+    (categories)=>categories.categories
         .reduce((acc,category)=>{
-        const {title,items}=category;
-        acc[title.toLowerCase()]=items;
-        return acc;
-        },{});
+            const {title,items}=category;
+            acc[title.toLowerCase()]=items;
+            return acc;
+        },{})
+);
