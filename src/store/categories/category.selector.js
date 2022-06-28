af167e02ea98a29af3cec171e0f0dd3cbd43efa4
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-06-28 12:51:01 -0500

    fixed issue with selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -9,10 +9,13 @@ export const selectCategories=createSelector(
 )
 
 export const selectCategoriesMap=createSelector([selectCategories],
-    (categories)=>categories.categories
-        .reduce((acc,category)=>{
-            const {title,items}=category;
-            acc[title.toLowerCase()]=items;
-            return acc;
-        },{})
+    (categories)=>{
+        return (categories.reduce(
+            (acc,category)=>{
+                const {title,items}=category;
+                acc[title.toLowerCase()]=items;
+                return acc;
+            },{}
+        ))
+    }
 );
