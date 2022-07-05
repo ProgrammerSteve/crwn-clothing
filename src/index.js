a787bab006702664651409508e8e6113065830c4
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-07-05 17:42:37 -0500

    used createRoot for ReactDOM for react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import {createRoot} from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.scss';
 import App from './App';
@@ -15,20 +16,20 @@ import { stripePromise } from './utils/stripe/stripe.utils';
 
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <Elements stripe={stripePromise}>
-            <App/>
-          </Elements>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Elements stripe={stripePromise}>
+          <App/>
+        </Elements>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+</React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
